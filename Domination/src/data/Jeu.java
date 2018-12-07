bd9244deac135beb1616a676c35e3b682d2e0e68
faucoant <37290943+faucoant@users.noreply.github.com>
2018-12-07 14:20:14 +0100

    Add files via upload

---
+++ Domination/src/data/Jeu.java
@@ -0,0 +1,26 @@
+package data;
+
+public class Jeu {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Player.coloR();
+		//Etape 1: Charger fichier
+		
+		
+		//Etape 2: Nombre de joueurs et sélection des rois. Choix couleur
+		
+		
+		//Etape 3: Création plateau
+		
+		//Etape 4: Débuter partie
+		
+		//Etape 5: Tour
+		
+		//Etape 6: Points
+		
+		//Etape 7: Fin du jeu
+	}
+
+}
