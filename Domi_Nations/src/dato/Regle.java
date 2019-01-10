828618394ab1b2b92668c6a2233c9268403dc9bb
faucoant <37290943+faucoant@users.noreply.github.com>
2019-01-10 11:38:30 +0100

    Add files via upload
    Version compter points

--- Domi_Nations/src/dato/Regle.java
+++ Domi_Nations/src/dato/Regle.java
@@ -1,89 +1,156 @@
-package dato;
+package com.company;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class Regle {
 
-	static int i = 0;
-	boolean equals;
+    static int i = 0;
+    boolean equals;
 
-	/**
-	 * Ce programme permet , en fonction du nombre de joueurs , de definir les
-	 * dominos restant
-	 * 
-	 * @param e
-	 * @return
-	 */
-	public static Map<String, ArrayList<String>> regleJoueur(int e) {
-		Map<String, ArrayList<String>> FullDomino = Domino.domino();
-		Map<String, ArrayList<String>> PoolDominos = new HashMap<String, ArrayList<String>>();
-		ArrayList<String> fullDom = new ArrayList<String>();
-		boolean equals;
-		if (e == 2) {
+    /**
+     * Ce programme permet , en fonction du nombre de joueurs , de definir les
+     * dominos restant
+     *
+     * @param e
+     * @return
+     */
+    public static Map<String, ArrayList<String>> regleJoueur(int e) {
+        Map<String, ArrayList<String>> FullDomino = Domino.domino();
+        Map<String, ArrayList<String>> PoolDominos = new HashMap<String, ArrayList<String>>();
+        ArrayList<String> fullDom = new ArrayList<String>();
+        boolean equals;
+        if (e == 2) {
 
-			int MaxDominos = 24; // Nombre total de dominos
-			int[] random = new int[MaxDominos];
-			while (i != MaxDominos) {
-				random[i] = ((int) (Math.random() * 47)) + 1; // Valeure al�atoire entre 1 et 48
-				equals = false;
-				for (int j = 0; j != i; j++) { // Verification des doublons
-					if (random[j] == random[i]) {
-						equals = true;
-					}
-				}
-				if (!equals) {
-					i++;
-				}
-			}
+            int MaxDominos = 24; // Nombre total de dominos
+            int[] random = new int[MaxDominos];
+            while (i != MaxDominos) {
+                random[i] = ((int) (Math.random() * 47)) + 1; // Valeure aléatoire entre 1 et 48
+                equals = false;
+                for (int j = 0; j != i; j++) { // Verification des doublons
+                    if (random[j] == random[i]) {
+                        equals = true;
+                    }
+                }
+                if (!equals) {
+                    i++;
+                }
+            }
 
-			for (int o = 0; o < random.length; o++) {
-				String u = String.valueOf(random[o]); // Change chaque entier en String
+            for (int o = 0; o < random.length; o++) {
+                String u = String.valueOf(random[o]); // Change chaque entier en String
 
-				fullDom = FullDomino.get(u); // Renvoit la liste correspondant � l'indice u <String>
-				PoolDominos.put(u, fullDom);
-				// System.out.println(u);
-				// jou.put("Joueur"+String.valueOf(random[o]),c)
-			}
+                fullDom = FullDomino.get(u); // Renvoit la liste correspondant à l'indice u <String>
+                PoolDominos.put(u, fullDom);
+                // System.out.println(u);
+                // jou.put("Joueur"+String.valueOf(random[o]),c)
+            }
 
-		}
-		if (e == 3) {
-			// m�me chose que precedement , sauf que MaxDominos = 36
-			int MaxDominos = 36;
-			int[] random = new int[MaxDominos];
-			while (i != MaxDominos) {
-				random[i] = ((int) (Math.random() * 47)) + 1;
-				equals = false;
-				for (int j = 0; j != i; j++) {
-					if (random[j] == random[i]) {
-						equals = true;
-					}
-				}
-				if (!equals) {
-					i++;
-				}
-			}
+        }
+        if (e == 3) {
+            // même chose que precedement , sauf que MaxDominos = 36
+            int MaxDominos = 36;
+            int[] random = new int[MaxDominos];
+            while (i != MaxDominos) {
+                random[i] = ((int) (Math.random() * 47)) + 1;
+                equals = false;
+                for (int j = 0; j != i; j++) {
+                    if (random[j] == random[i]) {
+                        equals = true;
+                    }
+                }
+                if (!equals) {
+                    i++;
+                }
+            }
 
-			for (int o = 0; o < random.length; o++) {
+            for (int o = 0; o < random.length; o++) {
 
-				String u = String.valueOf(random[o]);
+                String u = String.valueOf(random[o]);
 
-				fullDom = FullDomino.get(u);
-				PoolDominos.put(u, fullDom);
-			}
+                fullDom = FullDomino.get(u);
+                PoolDominos.put(u, fullDom);
+            }
 
-		}
+        }
 
-		if (e == 4) {
+        if (e == 4) {
 
-			PoolDominos = FullDomino;
+            PoolDominos = FullDomino;
 
-		}
-		System.out.println("Les dominos restants sont : " + PoolDominos);
-		return PoolDominos;
+        }
+        System.out.println("Les dominos restants sont : " + PoolDominos);
+        return PoolDominos;
 
-	}
+    }
+
+    public void calculscore(String joueur  Map<String, String[][]> PlateauJoueur){
+        String [][] tableaudomaine =  new String[9][9];
+        ArrayList<int[]> cordonees = new ArrayList<>();
+
+        List<String> nomdomaine = new Arrays.asList("Champs","Foret", "Mer", "Prairie", "Mine", "Montagne");
+        int[] tableaucordonees = new int[2];
+
+
+        String [][] plateaujoueur = PlateauJoueur.get(joueur);
+
+        int i=0;
+
+        while(i<nomdomaine.size()){
+            ArrayList<int[]> caseutilise = new ArrayList<>();
+            int count = 1;
+            for (int x= 0; x < 9  ; x++){
+                for (int y = 0 ; y < 9; y++){
+                    String domaine = Domino.domaine(PlateauJoueur[x][y]);
+
+                    if(domaine.equals(nomdomaine.get(i)) ) {
+                        tableaucordonees[0] = x;
+                        tableaucordonees[1] = y;
+                        tableaudomaine[x][y] = Plateaujoueur[x][y];//Plateau avec dominos du même domaine
+                        cordonees.add(tableaucordonees);//Liste composé de couple x,y. De chaque domino.
+                    }
+                    else{
+
+                        tableaudomaine = null;
+                    }
+
+                    for(int j=0; j<cordonees.size()/2; j++ ){//Parcours la liste cordonees, (2 par deux)
+
+                        ArrayList<int[]> couple = new ArrayList<>();
+                        couple.add((cordonees.get(2*j)));
+                        couple.add((cordonees.get(2*j+1))); //decoupe la liste en séprarant les couples
+                        int[] x = couple[0];//On redefini x à x //Problème!!!!!
+                        int[] y = couple[1];//On redefini y à y
+
+                        caseutilise.add(couple);
+
+                        ArrayList<int[]> couplereferant = new ArrayList<>();//Liste des couples referant
+                        couplereferant.add(new int[]{x-1,y});
+                        couplereferant.add(new int[]{x+1,y});
+                        couplereferant.add(new int[]{x,y-1});
+                        couplereferant.add(new int[]{x,y+1});
+
+                        for(int k=0; k<4; k++ ){//Taille de couple referant
+                            if(cordonees.contains(couplereferant.get(k))){
+                                int xans = couplereferant.get(k)[0];
+                                int yans = couplereferant.get(k)[1];
+                                count++;
+                                couplereferant.remove(couplereferant.get(k));
+                                x = xans;
+                                y = yans;
+
+                            }
+
+                        }
+
+                    }
+
+
+                    i++;
+                }
+            }
+        }
+    }
 
 }
+
