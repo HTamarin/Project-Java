014919068155ebe6a783e342753a651502d4eedd
segpayolo <45688835+segpayolo@users.noreply.github.com>
2019-01-13 19:58:00 +0100

    Add files via upload

---
+++ Domi_Nations/src/dato/AfficheRegle.java
@@ -0,0 +1,30 @@
+package dato;
+import java.awt.Font;
+import java.awt.GridLayout;
+
+import javax.swing.BoxLayout;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class AfficheRegle extends JPanel {
+	
+	
+	public AfficheRegle() {
+		GridLayout gl = new GridLayout();
+		gl.setColumns(1);
+		gl.setRows(2);
+		gl.setHgap(1);
+		gl.setVgap(1);;
+	this.setLayout(gl);
+		JLabel m = new JLabel("Manche "+ Turn.tour);
+		Font d = new Font("Dialog", Font.ITALIC, 75);
+	    m.setHorizontalAlignment(JLabel.CENTER);
+	    m.setFont(d);
+		JLabel t = new JLabel("Au tour du joueur "+Turn.joueuractuel);
+		t.setHorizontalAlignment(JLabel.CENTER);
+		t.setFont(d);
+		this.add(m);
+		this.add(t);
+	}
+
+}
